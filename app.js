249160452a688b5ee1623693739248bf1a1fa16b
Jaweki <werukioni@gmail.com>
2024-02-24 16:29:41 +0300

    added code line to allow cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import cors from 'cors'
 
 const port = 4000
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
+app.use(cors())
 
 io.on('connection', (socket) => {
   socket.on('disconnect', () => {
