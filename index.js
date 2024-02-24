f16cfdd8a253d1d2f1551a43f9b366dddb6f061f
Jaweki <werukioni@gmail.com>
2024-02-24 17:59:15 +0300

    added new code lines to index.js allowing websocket communication

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const { Server } = require('socket.io');
+const { createServer } = require('node:http')
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
 const PORT = process.env.PORT || 5001
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+server.use(express.static(path.join(__dirname, 'public')))
+server.set('views', path.join(__dirname, 'views'))
+server.set('view engine', 'ejs')
+server.get('/', (req, res) => res.render('pages/index'))
+server.get('/cool', (req, res) => res.send(cool()))
+
+io.on('connection', (socket) => {
+  console.log("new use connected to Websocket")
+
+  socket.on('disconnect', () => {
+    console.log('a user got disconnected')
+  })
+
+  socket.on('video_data', (videoChunk) => {
+    io.emit('broadcast_data', videoChunk)
+  })
+})
+
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
