2dc33023ffe21e10adf60c0d6b88eb467f415c97
Jaweki <werukioni@gmail.com>
2024-02-24 16:07:21 +0300

    content first deploy to heroku

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+const port = 4000
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('user got disconnected.')
+  })
+
+  socket.on('video_data', (videoChunk) => {
+    io.emit('broadcast_data', videoChunk)
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Websocket server created on port ${port}`)
+})
